d75ecf8411cb51fe642a9bea8f0ac69ed17647a8
xlinkexe <2457444033@qq.com>
2025-10-22 18:18:12 +0800

    feat: 在友情链接日志中添加青序栈被拒记录

--- src/site.config.ts
+++ src/site.config.ts
@@ -117,6 +117,7 @@ export const integ: IntegrationUserConfig = {
   links: {
     // Friend logbook
     logbook: [
+      { date: '2025-10-17', content: " [青序栈] rejected" },
       { date: '2025-10-17', content: " [清羽飞扬] added" },
       { date: '2025-10-10', content: " [Shuoer's blog] added" },
       { date: '2025-10-09', content: " [温锦瑜的博客]、[Chancel's blog]、[FKUN]、[Zeruns's Blog] added" }
